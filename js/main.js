d640039f3287422e1fa3fa9c0ced188a0a6af173
noahmagcalas <noah.magcalas@gmail.com>
2024-02-03 19:48:20 -0600

    fix: for real for real 404 pages

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 
     $("body").attr("class", "white-text");
 
-    $("#header").load("/pages/master.html header");
-    $("#footer").load("/pages/master.html footer");
+    $("#header").load("pages/master.html header");
+    $("#footer").load("pages/master.html footer");
 
     // inject bootstrap at bottom of body
     $("footer").after("<!-- bootstrap --> <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script>");
